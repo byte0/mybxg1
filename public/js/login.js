d4b29bbd83e62e5b49e087a61aaf21870b512f1a
code1031 <1031247267@qq.com>
2017-09-19 11:28:43 +0800

    模块化重构项目结构

---
+++ public/js/login.js
@@ -0,0 +1,22 @@
+define(['jquery','cookie'], function($) {
+  // 实现登录功能
+  $('#loginBtn').click(function() {
+    $.ajax({
+      type: 'post',
+      url: '/api/login',
+      data: $('#loginForm').serialize(),
+      dataType: 'json',
+      success: function(data) {
+        if (data.code == 200) {
+          // 存储用户登录之后的信息
+          $.cookie('loginInfo', JSON.stringify(data.result), {
+            path: '/'
+          });
+          // 登录成功，跳转到主页面
+          location.href = '/main/index';
+        }
+      }
+    });
+    return false; // 阻止默认行为
+  });
+});

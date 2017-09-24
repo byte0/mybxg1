554990efcd1a1922442e4970e74badf236101c5b
code1031 <1031247267@qq.com>
2017-09-24 10:01:24 +0800

    完成个人中心表单提交

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template','ckeditor','uploadify','region','datepicker','language'],function($,template,CKEDITOR){
+define(['jquery','template','ckeditor','uploadify','region','datepicker','language','validate','form'],function($,template,CKEDITOR){
   // 调用接口获取所有的个人信息
   $.ajax({
     type : 'get',
@@ -32,6 +32,35 @@ define(['jquery','template','ckeditor','uploadify','region','datepicker','langua
           { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
           { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] }
         ]
+      });
+      // 处理表单提交
+      $('#settingsForm').validate({
+        sendForm : false,
+        valid : function(){
+          // 拼接籍贯信息
+          var p = $('#p').find('option:selected').text();
+          var c = $('#c').find('option:selected').text();
+          var d = $('#d').find('option:selected').text();
+          var hometown = p + '|' + c + '|' + d;
+
+          // 更新富文本内容
+          for(var instance in CKEDITOR.instances){
+            CKEDITOR.instances[instance].updateElement();
+          }
+          // 提交表单
+          $(this).ajaxSubmit({
+            type : 'post',
+            url : '/api/teacher/modify',
+            data : {tc_hometown : hometown},
+            dataType : 'json',
+            success : function(data){
+              if(data.code == 200){
+                // 刷新当前页面
+                location.reload();
+              }
+            }
+          });
+        }
       });
     }
   });

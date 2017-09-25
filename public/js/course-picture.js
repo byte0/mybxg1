aa5b1e28f082ac88f509c7258d98c4b882076081
code1031 <1031247267@qq.com>
2017-09-25 11:53:27 +0800

    完成课程封面裁切功能

--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','uploadify'],function($,template,util){
+define(['jquery','template','util','uploadify','jcrop','form'],function($,template,util){
   // 设置导航菜单选中
   util.setMenu('/course/add');
   // 获取课程ID
@@ -29,6 +29,72 @@ define(['jquery','template','util','uploadify'],function($,template,util){
           $('.preview img').attr('src',obj.result.path);
         }
       });
+
+      // 选中图片
+      var img = $('.preview img');
+
+      // 图片裁切功能
+      $('#cropBtn').click(function(){
+        var flag = $(this).attr('data-flag');
+        if(flag){
+          // 提交页面
+          $('#cropForm').ajaxSubmit({
+            type : 'post',
+            url : '/api/course/update/picture',
+            data : {cs_id : csId},
+            dataType : 'json',
+            success : function(data){
+              if(data.code == 200){
+                location.href = '/course/lesson?cs_id=' + data.result.cs_id;
+              }
+            }
+          });
+        }else{
+          // 第一次点击按钮
+          $(this).text('保存图片').attr('data-flag',true);
+          // 实现图片裁切功能
+          cropImage();
+        }
+      });
+
+      // 封装一个独立的方法实现图片裁切
+      function cropImage(){
+        img.Jcrop({
+          aspectRatio : 2
+        },function(){
+          // 显示缩略图
+          this.initComponent('Thumbnailer',{width : 240,height : 120,mythumb:'.thumb'});
+
+          // 获取图片的宽度和高度
+          var width = this.ui.stage.width;
+          var height = this.ui.stage.height;
+
+          // 计算选区的数据
+          var x = 0;
+          var y = (height - width/2)/2;
+          var w = width;
+          var h = width/2;
+          // 创建一个选区
+          this.newSelection();
+          this.setSelect([x,y,w,h]);
+        });
+
+        // 监控选区的变化
+        img.parent().on('cropstart cropmove cropend',function(a,b,c){
+          // 选区完成和变化的时候把对应的坐标数据填充到表单里
+          var aInput = $('#cropForm').find('input');
+          aInput.eq(0).val(c.x);
+          aInput.eq(1).val(c.y);
+          aInput.eq(2).val(c.w);
+          aInput.eq(3).val(c.h);
+        });
+      }
+
+
+
+
+
+      
     }
   });
 

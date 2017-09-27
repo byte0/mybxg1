e5c968925848237c94c66ebd10ec3d392b14fb53
code1031 <1031247267@qq.com>
2017-09-27 11:00:30 +0800

    完成遮罩效果

---
+++ public/js/state.js
@@ -0,0 +1,15 @@
+define(['jquery'],function($){
+
+  $(document).ajaxStart(function(){
+    // 显示遮罩层
+    $('.overlay').show();
+  });
+
+  $(document).ajaxStop(function(){
+    // 显示遮罩层
+    setTimeout(function(){
+      $('.overlay').hide();
+    },500);
+  });
+
+});
